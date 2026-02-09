af2be4de4647dd5185fe78d40d5be05b96cb90b7
wonis1 <wjdwo2808@gmail.com>
2026-02-09 18:53:08 +0900

    sss

--- past_exams/11501_정재원.js
+++ past_exams/11501_정재원.js
@@ -1,18 +1,32 @@
 let example = `
-3
-3
-10 7 6
-3
-3 5 9
-5
-1 1 3 1 2
+5 150
+0 50 10
+0 50 20
+50 100 10
+100 151 10
+110 140 90
 `
 let data = example.trim();
-
 data = data.split('\n');
+let [number, totalLen] = data[0].split(' ').map(Number);
 data.shift();
-for (let i = 0; i < data.length; i+=2) {
-    let isShare = data[i+1].split(' ').map(Number);
-    let isMaxNum = Math.max.apply(null,isShare);
-    console.log(isMaxNum, isShare)
-}
+
+
+let method = [];
+data.map((n) => {
+    method.push(n.split(' ').map(Number));
+});
+
+method.sort((a,b) => {
+    return a[1] - b[1];
+});
+
+console.log(method);
+
+let saveLen = 0;
+method.reduce((acc, cur, idx) => {
+    let [start, end, shortcut] = method[idx];
+    acc = acc + end;
+    saveLen = 
+    
+}, 0)
